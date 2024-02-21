8519e02359501120ee386d8f1d7537c216393df1
Natmich <natmich6@gmail.com>
2024-02-22 00:13:15 +0300

    Second commiit

--- src/components/Home/Logo/logo.js
+++ src/components/Home/Logo/logo.js
@@ -1,46 +1,29 @@
-import './logo.scss'
-import Logon from '../../../assets/images/MGL2020.JPG'
-import { useEffect, useRef } from 'react'
-import DrawSVGPlugin from 'gsap-trial/DrawSVGPlugin'
-import gsap from 'gsap-trial'
+import './logo.scss';
+
+import Logon from '../../../assets/images/MGL2020.JPG';
+
+
 const Logo = () => {
+  // const bgRef = useRef();
+  // const solidLogoRef = useRef();
+  // const circleRef = useRef();
 
-    const bgRef = useRef()
-    const solidLogoRef = useRef()
-    const circleRef = useRef()
+  // useEffect(() => {
+  //   const bg = SVG(bgRef.current);
+  //   const solidLogo = SVG(solidLogoRef.current);
 
-useEffect (()=>{
-  gsap.registerPlugin(DrawSVGPlugin)
-  
-  gsap.timeline()
-  .to(bgRef.current, {
-    duration:1,
-    opacity:1
-  })
-  
-  gsap.fromTo(
-    solidLogoRef.current,
-    {
-      opacity:0,
-    },
-    {
-      opacity:1,
-      delay:1,
-      duration:6
-    }
-  )
-},[])
+  //   // Initial animations
+  //   bg.opacity(0).animate(1000).opacity(1);
+  //   solidLogo.opacity(0).animate(6000).opacity(1);
+  // }, []);
 
-
-
-    return (
-      
-        <div className='logo-container' ref={bgRef}>
-          <div className="circle" ref={circleRef}>
-            <img ref={solidLogoRef} className='solid-logo' src={Logon} alt='n'/>
-          </div>
-        </div>
-    )
-}
+  return (
+    <div className='logo-container' >
+      <div className="circle" >
+        <img className='solid-logo' src={Logon} alt='n' />
+      </div>
+    </div>
+  );
+};
 
 export default Logo;
--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Link } from 'react-router-dom';
+
 import LogoTitle from '../../assets/images/logo-n.png';
 import AnimatedLetters from '../AnimatedLetters/animatedletters';
 import Logo from "./Logo/logo";
